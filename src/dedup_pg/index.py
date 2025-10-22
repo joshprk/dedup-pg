0b6e5895f722c054ab0d943e54a1bbae2a0f21f6
Joshua Park <git@joshprk.me>
2025-10-21 21:10:32 -0400

    docs: add docstrings for index functions

--- src/dedup_pg/index.py
+++ src/dedup_pg/index.py
@@ -1,23 +1,23 @@
 import hashlib
 from collections.abc import Iterable
-from uuid import UUID, uuid4
+from uuid import UUID
 
 from .backend import LocalBackend
 
 
 class DedupIndex:
-    """
-    Indexing layer that allows for query-time deduplication through hashing.
-
-    Args:
-        num_perms (int): The number of permutation functions to use to generate item signatures
-        rows (int): The number of rows to use when making signature bands
-    """
     def __init__(
         self,
         num_perms: int = 128,
         rows: int = 5,
     ) -> None:
+        """
+        Indexing layer that allows for query-time deduplication through hashing.
+
+        Args:
+            num_perms (int): The number of permutation functions to use to generate item signatures
+            rows (int): The number of rows to use when making signature bands
+        """
         self.num_hashes = num_perms
         self.rows = rows
         self.num_bands = num_perms // rows
@@ -38,6 +38,14 @@ class DedupIndex:
         return signature
 
     def bands(self, tokens: Iterable[str]) -> list[str]:
+        """
+        Returns LSH bands. Currently, this only supports MinHash, which is the most popular algorithm for deduplicating
+        large-scale data.
+
+        Args:
+            tokens (Iterable[str]): An iterable of any byte-encodeable objects which represent the content to
+                deduplicate
+        """
         signature = self._minhash_signature(tokens)
         band_hashes: list[str] = []
 
@@ -50,8 +58,18 @@ class DedupIndex:
 
         return band_hashes
 
-    def items(self, band_hashes: list[str]) -> list[tuple[int, str]]:
-        return [(idx, bh) for idx, bh in enumerate(band_hashes)]
+    def items(self, bands: Iterable[str]) -> list[tuple[int, str]]:
+        """
+        A helper function which converts a list of bands to a normalized (index, band) format. Useful for inserting rows
+        into a database.
+
+        Args:
+            bands (Iterable[str]): An iterable of LSH bands
+        """
+        return [(idx, bh) for idx, bh in enumerate(bands)]
 
     def index(self, items: Iterable[tuple[int, str]]) -> UUID:
+        """
+        Retrieves the cluster UUID4 of the item generate
+        """
         return self._backend.insert(items)
